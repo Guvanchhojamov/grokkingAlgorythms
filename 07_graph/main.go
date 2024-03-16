66528d7a4d8fa4e11322e4f89c2d010a29161cd0
guvanchhojamov <guvanchojamov@gmail.com>
2024-03-16 18:55:13 +0500

    graph test problem

--- 07_graph/main.go
+++ 07_graph/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"slices"
 )
+
+type (
+	constType  map[string]int
+	parentType map[string]string
+	mgType     map[string]map[string]int
+)
+
+const infValue = 999999999
 
 func main() {
 	var mg = make(map[string]map[string]int)
@@ -11,8 +19,8 @@ func main() {
 	var costs = make(map[string]int)
 
 	mg["start"] = map[string]int{}
-	mg["start"]["a"] = 6
-	mg["start"]["b"] = 1
+	mg["start"]["a"] = 1
+	mg["start"]["b"] = 2
 
 	mg["b"] = map[string]int{}
 	mg["b"]["a"] = 3
@@ -25,10 +33,61 @@ func main() {
 	parents["a"] = "start"
 	parents["b"] = "start"
 
-	costs["a"] = 6
+	costs["a"] = 1
 	costs["b"] = 2
-	costs["end"] = int(math.Inf(1))
+	costs["end"] = infValue
 
-	fmt.Println(mg, parents, costs)
+	var proccessedNodes = []string{}
+	fmt.Println(mg, parents, costs, proccessedNodes)
 
+	//start    find short way to point
+	node := findLowestCostNode(costs, proccessedNodes)
+	for node != "" {
+		neigthbors := mg[node]
+		if len(neigthbors) == 0 {
+			break
+		}
+		cost := costs[node]
+		fmt.Println("cost:", cost, "neithbors", neigthbors, "node", node)
+		for n, _ := range neigthbors {
+			newCost := cost + mg[node][n]
+			if costs[n] > newCost {
+				costs[n] = newCost
+				parents[n] = node
+				proccessedNodes = append(proccessedNodes, node)
+			}
+		}
+		node = findLowestCostNode(costs, proccessedNodes)
+	}
+
+	fmt.Println("parents map:", parents)
+	fmt.Println(getWay(parents))
+	fmt.Println("finish")
+}
+func findLowestCostNode(costs map[string]int, processed []string) string {
+	minVal := infValue
+	var res = ""
+	for k, val := range costs {
+		if val < minVal && !slices.Contains(processed, k) {
+			minVal = val
+			res = k
+		}
+	}
+	return res
+}
+
+func getWay(parents map[string]string) string {
+	keys := []string{"b", "a", "end"}
+	var way string
+	var arrow string
+	for i, key := range keys {
+		if i != 0 {
+			arrow = "->"
+		}
+		way += fmt.Sprintf("%s%s", arrow, parents[key])
+	}
+	if len(way) == 0 {
+		return "way not found"
+	}
+	return way + "->end"
 }
